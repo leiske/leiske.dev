1056c159ca3f3b649a7119b56dbb602be94e6c4d
Colby <colby.leiske@gmail.com>
2026-01-02 18:21:24 -0800

    Implement calculateReadingTime function

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -4,3 +4,8 @@ import { readFileSync, readdirSync } from 'fs';
 import { join } from 'path';
 import type { Post, PostMeta } from '../types/post.js';
 
+export function calculateReadingTime(content: string): number {
+  const wordCount = content.split(/\s+/).length;
+  const wordsPerMinute = 200;
+  return Math.ceil(wordCount / wordsPerMinute);
+}
